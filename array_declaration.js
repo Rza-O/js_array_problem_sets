b1e44f8aa515fc2e8515ac4556cc7758ff35544e
appoclayptor <shahreza1414@gmail.com>
2024-08-31 20:25:24 +0600

    First problem completed

--- array_declaration.js
+++ array_declaration.js
@@ -3,3 +3,9 @@
  * 2 console log the 3rd index element
  * 3 change the value of the 2nd index element to jambura
 console log the final array */
+
+const fruits = ['apple', 'mango', 'guava', 'blueberry', 'jackfruit'];
+
+console.log(fruits);
+console.log(fruits[3]);
+console.log(fruits[2]);
