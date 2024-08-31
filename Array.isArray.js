4bb0f2e82b4ab18f076f28c665b0582ae4a5c064
appoclayptor <shahreza1414@gmail.com>
2024-08-31 20:43:17 +0600

    Fourth problem solved

--- Array.isArray.js
+++ Array.isArray.js
@@ -3,3 +3,11 @@
 Now use isArray to check if each variable is an array.
 
 Print a message to the console indicating whether each variable is an array or not. */
+
+const number = 13;
+const emotions = ['sad', 'angry', 'happy'];
+const users = [];
+
+console.log(Array.isArray(number));
+console.log(Array.isArray(emotions));
+console.log(Array.isArray(users));
