8c020309f2bd20b52a1af52df2672b6938ffa0bf
appoclayptor <shahreza1414@gmail.com>
2024-08-31 20:39:50 +0600

    Third problem solved

--- check-include.js
+++ check-include.js
@@ -3,3 +3,6 @@
 Use the includes method to check if the array contains a javascript book.
 
 Print a message to the console indicating whether the element is present in the array or not. */
+
+const books = ['Psychology of money', 'Rich dad poor dad', 'Minhajul abedin'];
+console.log(books.includes('JavaScript'), 'JavaScript book is not available');
